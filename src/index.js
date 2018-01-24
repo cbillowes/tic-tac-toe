4a634874f47d5e5e87f1419422203a8cca9cbe90
Clarice Bouwer <cbillowes@gmail.com>
2018-01-24 13:44:45 +0200

    Calculate and display the winning letter

--- src/index.js
+++ src/index.js
@@ -37,7 +37,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
@@ -78,7 +84,24 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
+function calculateWinner(squares) {
+  const possibilities = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < possibilities.length; i++) {
+    const [a, b, c] = possibilities[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+}
 
 ReactDOM.render(
   <Game />,
